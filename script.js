933f89b22c633096b31da62958b34c415258b9c5
stormslife <laimctindall@gmail.com>
2023-04-26 20:11:47 -0700

    Gallery Functionality Prototype

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-function imageEnlarge() {
-    var myImg = document.getElementById(id);
-    if (myImg.clientHeight == 500) {
-        return;
+//GALLERY
+
+var isEnlarged = false;
+var enlargedImage = "";
+
+function enlarge(id) {
+    
+    if (isEnlarged) {
+        document.getElementById(enlargedImage).style.height = "40%";
+        document.getElementById(enlargedImage).style.width = "10%";
+        document.getElementById(enlargedImage).style.position = "static";
+        document.getElementById(enlargedImage).style.top = "0";
+        document.getElementById(enlargedImage).style.left = "0";
+        
+        document.getElementById(id).style.height = "90%";
+        document.getElementById(id).style.width = "35%";
+        document.getElementById(id).style.position = "absolute";
+        document.getElementById(id).style.top = "100%";
+        document.getElementById(id).style.left = "30%";
     }
+
     else {
-        myImg.style.width = (currWidth + 400) + "px";
+        document.getElementById(id).style.height = "90%";
+        document.getElementById(id).style.width = "35%";
+        document.getElementById(id).style.position = "absolute";
+        document.getElementById(id).style.top = "100%";
+        document.getElementById(id).style.left = "30%";
+        isEnlarged = true;
     }
-    alert("func triggered")
+
+    enlargedImage = id;
 }
