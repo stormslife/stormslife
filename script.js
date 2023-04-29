1e376e2babed23296d0c69872cb20dd756e9cbf7
stormslife <laimctindall@gmail.com>
2023-04-29 10:49:02 -0700

    Uploaded article on food: apr 29

--- script.js
+++ script.js
@@ -6,24 +6,33 @@ var enlargedImage = "";
 function enlarge(id) {
     
     if (isEnlarged) {
-        document.getElementById(enlargedImage).style.height = "40%";
-        document.getElementById(enlargedImage).style.width = "10%";
-        document.getElementById(enlargedImage).style.position = "static";
-        document.getElementById(enlargedImage).style.top = "0";
-        document.getElementById(enlargedImage).style.left = "0";
-        
-        document.getElementById(id).style.height = "90%";
-        document.getElementById(id).style.width = "35%";
-        document.getElementById(id).style.position = "absolute";
-        document.getElementById(id).style.top = "100%";
-        document.getElementById(id).style.left = "30%";
+        if (enlargedImage == id) {
+            document.getElementById(enlargedImage).style.height = "70%";
+            document.getElementById(enlargedImage).style.width = "10%";
+            document.getElementById(enlargedImage).style.position = "static";
+            document.getElementById(enlargedImage).style.top = "0";
+            document.getElementById(enlargedImage).style.left = "0";
+        }
+        else {
+            document.getElementById(enlargedImage).style.height = "70%";
+            document.getElementById(enlargedImage).style.width = "10%";
+            document.getElementById(enlargedImage).style.position = "static";
+            document.getElementById(enlargedImage).style.top = "0";
+            document.getElementById(enlargedImage).style.left = "0";
+            
+            document.getElementById(id).style.height = "90%";
+            document.getElementById(id).style.width = "35%";
+            document.getElementById(id).style.position = "absolute";
+            document.getElementById(id).style.top = "70%";
+            document.getElementById(id).style.left = "30%";
+        }
     }
 
     else {
         document.getElementById(id).style.height = "90%";
         document.getElementById(id).style.width = "35%";
         document.getElementById(id).style.position = "absolute";
-        document.getElementById(id).style.top = "100%";
+        document.getElementById(id).style.top = "70%";
         document.getElementById(id).style.left = "30%";
         isEnlarged = true;
     }
